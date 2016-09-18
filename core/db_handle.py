e033bbeeee18d1d794d42d7d8bbd85c86fec34fb
Pitastic <mickep1985@gmail.com>
2016-09-18 16:16:18 +0200

    c

--- core/db_handle.py
+++ core/db_handle.py
@@ -123,4 +123,4 @@ if __name__ == "__main__":
 
 
 	# Start Service
-	run(app, host='0.0.0.0', port=8080, server=PasteServer, debug=True, reloader=True)
+	run(app, host='0.0.0.0', port=8080, server=PasteServer, debug=True)
